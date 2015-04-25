544d581beb3000790f9d183a3aa87c94686a5e10
Ronald Joo <rjoo189@gmail.com>
2015-04-26 08:55:08 +0900

    quick touches

--- js/name.js
+++ js/name.js
@@ -12,7 +12,20 @@
 		new Name({ fname: "Jon", lname: "Liebowitz"}),
 		new Name({ fname: "Steven", lname: "Colbert"}),
 		new Name({ fname: "Steve", lname: "Carrell"}),
-		new Name({ fname: "Wyatt", lname: "Cenac"})
+		new Name({ fname: "Wyatt", lname: "Cenac"}),
+		new Name({ fname: "Rob", lname: "Cordry"}),
+		new Name({ fname: "Jessica", lname: "Williams"}),
+		new Name({ fname: "John", lname: "Oliver"}),
+		new Name({ fname: "Samantha", lname: "Bee"}),
+		new Name({ fname: "Wolf", lname: "Blitzer"}),
+		new Name({ fname: "Anderson", lname: "Cooper"}),
+		new Name({ fname: "Megan", lname: "Kelly"}),
+		new Name({ fname: "Bill", lname: "O'Reilly"}),
+		new Name({ fname: "Connie", lname: "Chung"}),
+		new Name({ fname: "Mr.", lname: "G"}),
+		new Name({ fname: "Diane", lname: "Sawyer"}),
+		new Name({ fname: "Katie", lname: "Couric"}),
+
 	];
 
 	Name.search = function (searchQuery) {
